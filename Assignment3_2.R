9664c97952af1a05b23da2bf4321f8cdbd520420
VereDias <46394910+VereDias@users.noreply.github.com>
2019-02-25 15:57:40 +0530

    https://github.com/VereDias/Assignment3.2.git

---
+++ Assignment3_2.R
@@ -0,0 +1,17 @@
+#Assignment3.2
+
+m <- replicate(10, rnorm(10), simplify = "matrix")
+m <- as.data.frame(m)
+View(m)
+install.packages("rbenchmark")
+library(rbenchmark)
+benchmark(
+  vect = as.vector(m),
+  conc = (n <- as.vector(for (i in seq(nrow(m))) {
+    for (j in seq(ncol(m))) {
+      print(2*sin(m[i, j]))
+    }
+  }))
+)
+
+#VereDias
